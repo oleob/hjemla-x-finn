b61b52e9923eadae8fb5ec5c425351600a083b9a
Ole Barsch <ole.oystein.barsch@nrk.no>
2024-03-21 11:54:35 +0100

    open in new tab

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,7 @@
   export let url: string;
 </script>
 
-<a href={url} class="button button--flat">
+<a href={url} class="button button--flat" target="_blank" referrerpolicy="no-referrer">
   <img
     src="https://theme.zdassets.com/theme_assets/9593466/eea3b813d78d0d82f7405b9c8c04c1b322a51dcd.png"
     alt="Hjemla logo" />
